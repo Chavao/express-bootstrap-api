96ce48eb2e6fd382d83ca46353986fdba4088e52
Diego Chavão <diegochavao@gmail.com>
2017-01-28 11:34:24 -0200

    Minimal Express API application.

---
+++ app.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import logger from 'morgan';
+
+// Importing routes
+import index from './routes/index';
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+
+// Routes
+app.use('/', index);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Development error handler
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// Production error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
